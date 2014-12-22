5dddd1f4b3bf8cf58aab87a24949a00103c4add3
Alex Harden <aharden@te.com>
2014-12-22 13:35:00 -0500

    PUP-40 initial implementation of backup rsync jobs
    PUP-40 fixed file attribute user -> owner
    
    PUP-40 added keyfile attribute

---
+++ manifests/backup.pp
@@ -0,0 +1,39 @@
+# profile to be applied to the Puppet project backup server (aharden@te.com)
+# should be included in roles::puppet::backup
+class te_puppet::backup (
+  $backup_path,
+  $destinations,
+) {
+  include ::rsync
+  include ::rsync::server
+
+  # setup PE server backup repo
+
+  File {
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { $backup_path: }
+
+  #source: http://stackoverflow.com/questions/21882525/puppet-recursive-directories-creation
+  define myDirectories (
+    $myBackupPath = $te_puppet::backup::backup_path,
+  ) {
+    file { "$myBackupPath/$name":
+      ensure => directory,
+    }
+  }
+  myDirectories { $destinations: }
+
+  rsync::server::module { 'PE_repo':
+    path    => $backup_path,
+    require => File[$backup_path],
+  }
+
+  service { 'rsync':
+    ensure => 'running',
+    enable => true,
+  }
+}
