dd54ca54f122a163046a240d79004a4f40414415
Alex Harden <aharden@te.com>
2014-12-22 14:04:19 -0500

    PUP-40 adjusted configs to point to backup directory instead of tmp

--- manifests/common.pp
+++ manifests/common.pp
@@ -1,8 +1,16 @@
 # a profile to be referenced by all Puppet Enterprise server roles (aharden@te.com)
-class te_puppet::common {
+class te_puppet::common (
+  $backup_dir,
+) {
+  file { "$backup_dir":
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+  }
+
   cron { '/etc/puppetlabs backup':
     ensure  => present,
-    command => ". /root/.bashrc; tar -czpf /tmp/$::hostname.etc.puppetlabs.tgz /etc/puppetlabs",
+    command => ". /root/.bashrc; tar -czpf /$backup_dir/$::hostname.etc.puppetlabs.tgz /etc/puppetlabs",
     user    => 'root',
     hour    => 1,
     minute  => 0,
