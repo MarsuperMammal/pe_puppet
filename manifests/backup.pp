409c471f2324363742fc840511dac5778c7ce22d
Alex Harden <aharden@te.com>
2015-01-08 14:46:13 -0500

    debug

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -1,7 +1,7 @@
 # profile to be applied to the Puppet project backup server (aharden@te.com)
 # should be included in roles::puppet::backup
 class te_puppet::backup (
-  $backup_path = $::te_puppet::common::rsync_dest_path,
+  $backup_path = $te_puppet::common::rsync_dest_path,
 ) {
   include ::rsync
   include ::rsync::server
