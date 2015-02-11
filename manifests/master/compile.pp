181bac222d4f57532bc6e91dbd1c7c5a1b1a8e74
Alex Harden <aharden@te.com>
2015-02-11 13:48:55 -0500

    PUP-255 moved deprecated compile.pp to PE 3.3 only

--- manifests/master/compile.pp
+++ manifests/master/compile.pp
@@ -1,54 +1,61 @@
 # profile to apply to Puppet Enterprise masters not acting as a CA (aharden@te.com)
+# deprecated as of PE 3.7: https://docs.puppetlabs.com/pe/latest/install_multimaster.html
 class te_puppet::master::compile {
   include ::te_puppet::master
 
-  ini_setting { 'ca_false':
-    ensure  => present,
-    path    => $::settings::config,
-    section => 'main',
-    setting => 'ca',
-    value   => false,
-  }
+  case $::pe_version {
+    '3.3.2': {
 
-  File {
-    ensure => file,
-    owner  => 'pe-puppet',
-    group  => 'pe-puppet',
-    mode   => '0644',
-  }
+      ini_setting { 'ca_false':
+        ensure  => present,
+        path    => $::settings::config,
+        section => 'main',
+        setting => 'ca',
+        value   => false,
+      }
 
-  file { "${::settings::confdir}/ssl/ca":
-    ensure => directory,
-  }
+      File {
+        ensure => file,
+        owner  => 'pe-puppet',
+        group  => 'pe-puppet',
+        mode   => '0644',
+      }
 
-  file { "${::settings::confdir}/ssl/ca/ca_crl.pem":
-    ensure => file,
-    source => 'puppet:///puppet_ssl/ca/ca_crl.pem',
-    notify => Service['pe-httpd'],
-  }
+      file { "${::settings::confdir}/ssl/ca":
+        ensure => directory,
+      }
 
-  file { "${::settings::ssldir}/public_keys/pe-internal-mcollective-servers.pem":
-    source => "puppet://${::settings::ca_server}/puppet_ssl/public_keys/pe-internal-mcollective-servers.pem",
-  }
+      file { "${::settings::confdir}/ssl/ca/ca_crl.pem":
+        ensure => file,
+        source => 'puppet:///puppet_ssl/ca/ca_crl.pem',
+        notify => Service['pe-httpd'],
+      }
 
-  file { "${::settings::ssldir}/public_keys/pe-internal-peadmin-mcollective-client.pem":
-    source => "puppet://${::settings::ca_server}/puppet_ssl/public_keys/pe-internal-peadmin-mcollective-client.pem",
-  }
+      file { "${::settings::ssldir}/public_keys/pe-internal-mcollective-servers.pem":
+        source => "puppet://${::settings::ca_server}/puppet_ssl/public_keys/pe-internal-mcollective-servers.pem",
+      }
 
-  file { "${::settings::ssldir}/public_keys/pe-internal-puppet-console-mcollective-client.pem":
-    source => "puppet://${::settings::ca_server}/puppet_ssl/public_keys/pe-internal-puppet-console-mcollective-client.pem",
-  }
+      file { "${::settings::ssldir}/public_keys/pe-internal-peadmin-mcollective-client.pem":
+        source => "puppet://${::settings::ca_server}/puppet_ssl/public_keys/pe-internal-peadmin-mcollective-client.pem",
+      }
 
-  file { "${::settings::ssldir}/private_keys/pe-internal-mcollective-servers.pem":
-    mode   => '0640',
-    source => "puppet://${::settings::ca_server}/puppet_ssl/private_keys/pe-internal-mcollective-servers.pem",
-  }
+      file { "${::settings::ssldir}/public_keys/pe-internal-puppet-console-mcollective-client.pem":
+        source => "puppet://${::settings::ca_server}/puppet_ssl/public_keys/pe-internal-puppet-console-mcollective-client.pem",
+      }
 
-  file { "${::settings::ssldir}/certs/pe-internal-mcollective-servers.pem":
-    source => "puppet://${::settings::ca_server}/puppet_ssl/certs/pe-internal-mcollective-servers.pem",
-  }
+      file { "${::settings::ssldir}/private_keys/pe-internal-mcollective-servers.pem":
+        mode   => '0640',
+        source => "puppet://${::settings::ca_server}/puppet_ssl/private_keys/pe-internal-mcollective-servers.pem",
+      }
 
-  file { "${::settings::ssldir}/certs/ca.pem":
-    source => "puppet://${::settings::ca_server}/puppet_ssl/certs/ca.pem",
+      file { "${::settings::ssldir}/certs/pe-internal-mcollective-servers.pem":
+        source => "puppet://${::settings::ca_server}/puppet_ssl/certs/pe-internal-mcollective-servers.pem",
+      }
+
+      file { "${::settings::ssldir}/certs/ca.pem":
+        source => "puppet://${::settings::ca_server}/puppet_ssl/certs/ca.pem",
+      }
+    }
+  default : {}
   }
 }
