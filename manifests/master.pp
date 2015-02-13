3773c1fc9044ed04d12649b4d3d5470121c4c3f2
Alex Harden <aharden@te.com>
2015-02-13 11:41:12 -0500

    PUP-255 Add daemon package on Debian PE masters

--- manifests/master.pp
+++ manifests/master.pp
@@ -51,4 +51,13 @@ class te_puppet::master (
     target => '/opt/puppet/bin/puppet',
     path   => '/usr/bin/puppet',
   }
+  
+  # daemon is used in r10k webhook on debian/ubuntu
+  case ::osfamily {
+    'Debian': {
+      package { 'daemon':
+        ensure => 'latest',
+      }
+    }
+  }
 }
