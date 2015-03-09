adbb11057f8f82ae8c2a6207abf6c6591ef6b561
Alex Harden <aharden@te.com>
2015-03-09 11:16:49 -0400

    PUP-293 agent::install adjustments

--- manifests/agent/install.pp
+++ manifests/agent/install.pp
@@ -11,11 +11,11 @@ class te_puppet::agent::install (
     'solaris': {}
     'windows': {
       case $::architecture {
-        ‘x86’: {
+        'x86': {
           $package_msi  = "puppet-enterprise-${version}.msi"
           $package_name = 'Puppet Enterprise'
         }
-        ‘x64’: {
+        'x64': {
           $package_msi  = "puppet-enterprise-${version}-x64.msi"
           $package_name = 'Puppet Enterprise (64-bit)'
         }
@@ -27,7 +27,7 @@ class te_puppet::agent::install (
       $package_source  = "${source}\\${version}\\${package_msi}"
 
       $provider        = 'windows' #default for ::osfamily windows
-      $install_options = [‘PUPPET_MASTER_SERVER=$::settings::ca_server’]
+      $install_options = ["PUPPET_MASTER_SERVER=$::settings::ca_server"]
     }
     default: {
       notify { "Unsupported OS family ${::osfamily}.": }
