b26027d6e5a25d1a64d6a8b15a7272bdce850098
Alex Harden <aharden@te.com>
2015-04-02 14:20:47 -0400

    PUP-242 added old db backup cleanup

--- manifests/db.pp
+++ manifests/db.pp
@@ -11,6 +11,18 @@ class te_puppet::db (
     minute => '0',
   }
 
+# cleaning up after abandoned backups from old code
+  File {
+    ensure => 'absent',
+  }
+
+  file { "${bkup_folder}console.backup":}
+  file { "${bkup_folder}pe-activity.backup":}
+  file { "${bkup_folder}pe-classifier.backup":}
+  file { "${bkup_folder}pe-puppetdb.backup":}
+  file { "${bkup_folder}pe-rbac.backup":}
+# end cleaning up (remove in next version)
+
   file { $bkup_folder:
     ensure => 'directory',
     mode   => '0775',
@@ -21,6 +33,6 @@ class te_puppet::db (
   # cron job to produce full DB backup on disk
   # https://docs.puppetlabs.com/pe/3.7/maintain_console-db.html#database-backups
   cron { 'Full DB backup':
-    command => "/opt/puppet/bin/pg_dumpall -f ${bkup_folder}fulldb.backup",
+    command => "/opt/puppet/bin/pg_dumpall -f ${bkup_folder}dumpall.backup",
   }
 }
